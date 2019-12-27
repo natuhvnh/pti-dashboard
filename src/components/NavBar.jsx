bd88d1149f49fcc557dd9327ab626eedb5968a69
natuhvnh <natuhvnh@gmail.com>
2019-12-27 15:58:45 +0700

    add icon folder

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,27 @@ import React, { Component } from "react";
 
 class NavBar extends Component {
   render() {
-    return <div className="">NavBar</div>;
+    return (
+      <div className="nav-display">
+        <div className="nav-img">
+          <div className="img">
+            <img src="images/brand.png" alt="brand" />
+          </div>
+        </div>
+        <form action="" className="nav-form">
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="Nhập từ khóa, vd: tìm mã hợp đồng"
+              className="search-input"
+            />
+            <button className="search-button">
+              <i className="fa fa-search" aria-hidden="true"></i>
+            </button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
