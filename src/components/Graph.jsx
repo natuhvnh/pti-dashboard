8cd13966cb358f57be52c2dbc34e35f26adb459f
natuhvnh <natuhvnh@gmail.com>
2019-12-30 17:05:34 +0700

    Add Graph component

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -2,7 +2,25 @@ import React, { Component } from "react";
 
 class Graph extends Component {
   render() {
-    return <div>Graph</div>;
+    return (
+      <div>
+        <div className="graph-header">
+          <div className="graph-note">
+            <h6>Thống kê</h6>
+            <span>
+              <i className="fa fa-circle thanh-toan"></i>Thanh toán
+            </span>
+            <span>
+              <i className="fa fa-circle hoa-hong"></i>Hoa hồng
+            </span>
+            <span>
+              <i className="fa fa-circle boi-thuong"></i>Bồi thường
+            </span>
+          </div>
+          <div className="graph-option">Lua chon</div>
+        </div>
+      </div>
+    );
   }
 }
 
